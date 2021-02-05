7606dca97191af32fc74cc0d57fd132dacca69cd
YueZhang1027 <47068605+YueZhang1027@users.noreply.github.com>
2021-02-05 18:23:06 +0800

    Init-2

---
+++ Assets/Scripts/Player/PlayerJump.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    Rigidbody rigid;
+    Collider col;
+
+    public float jumpPower = 15;
+
+    void Awake()
+    {
+        rigid = this.GetComponentInParent<Rigidbody>();
+        col = this.GetComponent<Collider>();
+    }
+
+    void Update()
+    {
+        Vector3 newVelocity = rigid.velocity;
+
+        if (Input.GetKeyDown(KeyCode.Z) && IsGrounded())
+            newVelocity.y = jumpPower;
+
+        rigid.velocity = newVelocity;
+    }
+
+    bool IsGrounded()
+    {
+        Ray ray = new Ray(col.bounds.center, Vector3.down);
+        float radius = col.bounds.extents.x - .05f;
+        float fullDistance = col.bounds.extents.y + .05f;
+
+        if (Physics.SphereCast(ray, radius, fullDistance)) return true;
+        else return false;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerRun.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRun : MonoBehaviour
+{
+    Rigidbody rigid;
+
+    public float moveSpeed = 5;
+
+    void Awake()
+    {
+        rigid = this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 newVelocity = rigid.velocity;
+
+        newVelocity.x = Input.GetAxis("Horizontal") * moveSpeed;
+
+        rigid.velocity = newVelocity;
+    }
+}
